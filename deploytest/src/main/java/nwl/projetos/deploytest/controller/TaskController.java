eb9f1dbb3c04715db45e45025b57c787a2a5f9ac
Norwall Neto <norwalgomess@gmail.com>
2026-08-29 20:12:41 -0300

    Complentado o TaskController

--- deploytest/src/main/java/nwl/projetos/deploytest/controller/TaskController.java
+++ deploytest/src/main/java/nwl/projetos/deploytest/controller/TaskController.java
@@ -1,8 +1,14 @@
 package nwl.projetos.deploytest.controller;
 
+import jakarta.validation.Valid;
+import nwl.projetos.deploytest.domain.dto.TaskRequestDTO;
+import nwl.projetos.deploytest.domain.dto.TaskResponseDTO;
 import nwl.projetos.deploytest.service.TaskService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -12,5 +18,42 @@ public class TaskController {
 
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskResponseDTO> criar(
+            @Valid @RequestBody TaskRequestDTO request
+    ) {
+        TaskResponseDTO response = taskService.criar(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskResponseDTO>> listarTodos() {
+        return ResponseEntity.ok(taskService.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskResponseDTO> buscarPorId(
+            @PathVariable Long id
+    ) {
+        return ResponseEntity.ok(taskService.buscarPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskResponseDTO> atualizar(
+            @PathVariable Long id,
+            @Valid @RequestBody TaskRequestDTO request
+    ) {
+        return ResponseEntity.ok(taskService.atualizar(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        taskService.excluir(id);
+        return ResponseEntity.noContent().build();
     }
 }
