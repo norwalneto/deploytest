ae6e72a622f1621774c02f5e0eca428dbd1d753e
Norwall Neto <norwalgomess@gmail.com>
2026-08-29 18:22:55 -0300

    Primeiro commit

---
+++ deploytest/src/main/java/nwl/projetos/deploytest/DeploytestApplication.java
@@ -0,0 +1,13 @@
+package nwl.projetos.deploytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploytestApplication.class, args);
+	}
+
+}
---
+++ deploytest/src/test/java/nwl/projetos/deploytest/DeploytestApplicationTests.java
@@ -0,0 +1,13 @@
+package nwl.projetos.deploytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
