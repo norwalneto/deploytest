5808a971b03a07ca3aac46130b7aa94aafef2ba8
Norwall Neto <norwalgomess@gmail.com>
2026-08-29 19:13:53 -0300

    deploy teste coloque o @RequestMapping

--- deploytest/src/main/java/nwl/projetos/deploytest/controller/DeployTestController.java
+++ deploytest/src/main/java/nwl/projetos/deploytest/controller/DeployTestController.java
@@ -1,9 +1,11 @@
 package nwl.projetos.deploytest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class DeployTestController {
 
     @GetMapping("/home")
